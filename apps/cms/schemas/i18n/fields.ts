afb62dffedc51230d48d91cc90c7a42cb4bd7d93
Federico Minaya <federico.minaya@aerolab.co>
2023-03-06 17:13:53 -0300

    fix: add tscheck to define type in lang fields

--- apps/cms/schemas/i18n/fields.ts
+++ apps/cms/schemas/i18n/fields.ts
@@ -1,29 +1,31 @@
 import {supportedLanguages} from './lang'
+import {defineType} from 'sanity'
 
 const capitalize = (str: string, lower = false) =>
   (lower ? str.toLowerCase() : str).replace(/(?:^|\s|["'([{])+\S/g, (match) => match.toUpperCase())
 
-export const createLocalizedField = (type: string) => ({
-  title: `Localized ${type}`,
-  name: `locale${capitalize(type)}`,
-  type: 'object',
-  // Fieldsets can be used to group object fields.
-  // Here we omit a fieldset for the "default language",
-  // making it stand out as the main field.
-  fieldsets: [
-    {
-      title: 'Translations',
-      name: 'translations',
-      options: {collapsible: true, collapsed: true},
-    },
-  ],
-  // Dynamically define one field per language
-  fields: supportedLanguages.map((lang) => ({
-    title: lang.title,
-    name: lang.id,
-    type: type,
-    fieldset: lang.isDefault ? null : 'translations',
-  })),
-})
+export const createLocalizedField = (type: string) =>
+  defineType({
+    title: `Localized ${type}`,
+    name: `locale${capitalize(type)}`,
+    type: 'object',
+    // Fieldsets can be used to group object fields.
+    // Here we omit a fieldset for the "default language",
+    // making it stand out as the main field.
+    fieldsets: [
+      {
+        title: 'Translations',
+        name: 'translations',
+        options: {collapsible: true, collapsed: true},
+      },
+    ],
+    // Dynamically define one field per language
+    fields: supportedLanguages.map((lang) => ({
+      title: lang.title,
+      name: lang.id,
+      type: type,
+      fieldset: lang.isDefault ? undefined : 'translations',
+    })),
+  })
 
 export const i18nFields = [createLocalizedField('string'), createLocalizedField('text')]
